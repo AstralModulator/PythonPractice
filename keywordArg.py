fc5565be32f9cf83ab6d437be3bc164fb0e8ccb5
Dip Kiran Limbu <dip.231512@ncit.edu.np>
2025-11-29 18:28:34 +0545

    Keyword Arguments

--- keywordArg.py
+++ keywordArg.py
@@ -3,4 +3,4 @@
 def greet_user(greetings, title, first, last):
     return f"{greetings} {title} {first} {last}"
 
-print(greet_user(title = "Mrs.",greetings = "Namaste!", last = "Limbu", first = "Dip Jyoti"))
+print(greet_user("Hello!",title = "Mrs.", last = "Limbu", first = "Dip Jyoti"))
