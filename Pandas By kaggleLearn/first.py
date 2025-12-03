ec10bfe30f1bb3ee4af9e59652f08aa7fcd05824
Dip Kiran Limbu <dip.231512@ncit.edu.np>
2025-12-03 12:42:11 +0545

    Hangman Game and a simple encryption Program

--- Pandas By kaggleLearn/first.py
+++ Pandas By kaggleLearn/first.py
@@ -1,6 +1,1 @@
-import pandas as pd
-
-pd.DataFrame({'Bob':['I liked it','It was awful'],'Sue':['Pretty good','Bland']},index = ['Product A','Product B'])
-pd.Series([1,2,3,4,5])
-
-pd.Series([1,2,3,4,5],index=['Product A','Product B','Product C','Product D','Product F'],name = "Product E")
+#First Exercise For kaggle
