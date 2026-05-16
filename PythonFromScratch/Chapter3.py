951cf560b7beb6b5861be73fe5204a6ea9f5b567
Dip Kiran Limbu <deepkiranchemjong101@gmail.com>
2026-05-16 23:41:41 +0000

    Strings

---
+++ PythonFromScratch/Chapter3.py
@@ -0,0 +1,12 @@
+print("Hello\' World!!")
+
+print(r"Raw \n String")
+print('Raw \t String \n Whats up')
+
+print("""Raw \
+Raw""")
+print("My name is 'Dip' Kiran")
+s = "deep"
+s.capitalize()
+print(s)
+
